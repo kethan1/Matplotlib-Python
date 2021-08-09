ba77228e736ae8d10d59a78c2f500d35397116bd
Kethan Vegunta <kethan@vegunta.com>
2021-08-09 13:01:59 -0700

    finish matplotlib csv

--- matplotlib_csv.py
+++ matplotlib_csv.py
@@ -3,18 +3,24 @@ import csv
 
 x = []
 y = []
-with open("US_chronic_diseases.csv") as input_file:
+with open('US_chronic_diseases.csv') as input_file:
     print(input_file.readline().split(','))
-    plots = csv.reader(input_file, delimiter=",")
+    plots = csv.reader(input_file, delimiter=',')
+    rows = 0
+    idaho_alcohol = 0
+    male_arthritis = 0
     for row in plots:
-        print(row)
-        if row[5].lower() == 'alcohol' and (int(row[14]) - int(row[15])) >= 3:
-            print('alcohol')
-        x.append(int(row[0]))
-        y.append(int(row[1]))
+        rows += 1
+        # print(row)
+        if row[5].lower() == 'alcohol' and row[3].lower() == 'idaho':
+            idaho_alcohol += 1
+        elif 'Gender' in row and row[row.index('Gender') + 1] == 'Male' and row[5] == 'Arthritis':
+            male_arthritis += 1
 
-plt.plot(x, y, label="Loaded from file")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.title("MyProject")
+plt.pie(
+    [rows - (idaho_alcohol + male_arthritis), idaho_alcohol, male_arthritis],
+    labels=['other', 'Alcoholic Idahos', 'Males with Arthritis'], startangle=90,
+    explode=(0, 0.2, 0.2), autopct='%1.1f%%'
+)
+plt.tight_layout()
 plt.show()
